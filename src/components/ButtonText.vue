c21d5d727e5b4a6458f863a9944e2f86658a30e6
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-08 19:38:32 +0200

    Create button prorp

---
+++ src/components/ButtonText.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    props: {
+        title: String,
+        iconButton: String,
+    }
+}
+</script>
+
+<template>
+    <button>
+        <span class="title_button">
+            {{ title }}
+        </span>
+        <!-- Aggiungo la favicon quando serve -->
+        <i v-if="iconButton" :class="iconButton"></i>
+    </button>
+</template>
+
+<style>
+button {
+    margin: 2rem;
+    padding: 1rem 2rem;
+    font-size: 1rem;
+    color: var(--rax-darkest);
+    background-color: var(--rax-secondary);
+    border-radius: 0.5rem;
+    border: none;
+
+
+    .title_button {
+        padding-right: 0.3rem;
+    }
+}
+
+button:hover {
+    background-color: transparent;
+    border: 1px solid var(--rax-secondary);
+    color: var(--rax-secondary);
+}
+</style>
