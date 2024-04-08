f447112aea66917b70794e2c3c5e9dbe33c70f9c
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-08 19:39:45 +0200

    Add hero text and button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,12 @@
 <script>
 import NavigationBar from "./NavigationBar.vue";
+import ButtonText from "./ButtonText.vue"
 
 export default {
     name: 'AppHeader',
     components: {
         NavigationBar,
+        ButtonText,
     }
 }
 </script>
@@ -13,7 +15,15 @@ export default {
     <header>
         <div class="container">
             <NavigationBar />
+            <div class="hero d_flex">
+                <div>
+                    <h4 id="welcome">Welcome to raxg</h4>
+                    <h2>Are youready for your next challenge?</h2>
+                    <ButtonText title="Read More" iconButton="fa-solid fa-right-long" />
+                </div>
+            </div>
         </div>
+
     </header>
 </template>
 
@@ -22,5 +32,22 @@ header {
     background-image: url(public/img/banner1.png);
     width: 100%;
     height: 900px;
+    color: var(--rax-light);
+}
+
+.hero {
+    text-align: center;
+    justify-content: center;
+    height: 700px;
+    text-transform: uppercase;
+
+    #welcome {
+        color: var(--rax-secondary);
+        font-size: 1rem;
+    }
+
+    & h2 {
+        font-size: 4rem;
+    }
 }
 </style>
