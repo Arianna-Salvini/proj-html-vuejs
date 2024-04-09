42fcb81eec7c6c72ee452318761da56e9afe0f16
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-09 13:02:21 +0200

    Create component for title of section and title of description

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props: {
+        titleSection: String,
+        title: String,
+    }
+}
+</script>
+
+<template>
+    <h5 class="section_title"> {{ titleSection }}</h5>
+    <h2 class="title_text">{{ title }}</h2>
+</template>
+
+<style>
+.section_title {
+    color: var(--rax-secondary);
+    text-transform: uppercase;
+    font-size: 1rem;
+    padding-bottom: 1.5rem;
+}
+
+.title_text {
+    font-size: 2.9rem;
+    text-transform: capitalize;
+}
+</style>
