3d7dfe59ea3838b63e0504580a6f6f0d0ad574d0
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-09 19:43:10 +0200

    Add titles of  section

---
+++ src/components/section/UpcomingMatch.vue
@@ -0,0 +1,22 @@
+<script>
+import ButtonText from "../elements/ButtonText.vue";
+import ButtonPlay from "../elements/ButtonPlay.vue";
+import SectionTitle from "../elements/SectionTitle.vue";
+
+
+export default {
+    name: 'UpcomingMatch',
+    components: {
+        ButtonText,
+        ButtonPlay,
+        SectionTitle,
+    },
+}
+</script>
+
+<template>
+</template>
+
+<script>
+
+</script>
