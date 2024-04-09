87ab23164117b010afdec3ff93bd7ba78f642a60
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-09 17:01:35 +0200

    Add elemnt for next button, back buttoon and generic button

---
+++ src/components/elements/ButtonBackNext.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'ButtonBack',
+}
+</script>
+
+<template>
+    <button class="play_btn">
+        <div class="play_btn_icon">
+            <i class="fa-solid fa-arrow-left-long"></i>
+        </div>
+    </button>
+</template>
+
+<style scoped>
+.play_btn {
+    width: 3.3rem;
+    aspect-ratio: 1;
+    color: var(--rax-darkest);
+    background-color: var(--rax-light);
+    border-radius: 50%;
+    border: none;
+
+    .play_btn_icon {
+        font-size: 1rem;
+    }
+
+}
+
+.play_btn:hover {
+    background-color: transparent;
+    border: 1px solid var(--rax-highlight);
+    color: var(--rax-light);
+}
+</style>
---
+++ src/components/elements/ButtonGeneric.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: 'ButtonGeneric',
+    props: {
+        genericIcon: String,
+    }
+}
+</script>
+
+<template>
+    <button class="play_btn">
+        <div class="play_btn_icon">
+            <i :class="genericIcon"></i>
+        </div>
+    </button>
+</template>
+
+<style scoped>
+.play_btn {
+    width: 3.3rem;
+    aspect-ratio: 1;
+    color: var(--rax-darkest);
+    background-color: var(--rax-light);
+    border-radius: 50%;
+    border: none;
+
+    .play_btn_icon {
+        font-size: 1rem;
+    }
+
+}
+
+.play_btn:hover {
+    background-color: transparent;
+    border: 1px solid var(--rax-highlight);
+    color: var(--rax-light);
+}
+</style>
