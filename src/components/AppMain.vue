e43032a1c7bf664cc9e5f19a6550aa82e35d6a95
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-09 19:58:39 +0200

    Add section trending Streaming Add titles section and  back and next button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,8 +16,7 @@ export default {
         UpcomingMatch,
         TrendingBattle,
         TrendingGames,
-    },
-
+    }
 }
 </script>
 
---
+++ src/components/section/TrendingGames.vue
@@ -0,0 +1,56 @@
+<script>
+import ButtonNext from "../elements/ButtonNext.vue";
+import ButtonBack from "../elements/ButtonBack.vue";
+import ButtonPlay from "../elements/ButtonPlay.vue";
+import SectionTitle from "../elements/SectionTitle.vue";
+
+export default {
+    name: 'TrendingGames',
+    components: {
+        ButtonNext,
+        ButtonBack,
+        ButtonPlay,
+        SectionTitle,
+    },
+}
+</script>
+
+<template>
+    <section class="trending_games">
+        <div class="container ">
+            <div class="top_section d_flex">
+                <div class="half_container">
+                    <div class="titles">
+                        <SectionTitle title="Checkout our llive streaming" titleSection="Live Streaming" />
+                    </div>
+                </div>
+                <div class="half_container .right">
+                    <div class="button_box d_flex ">
+                        <ButtonBack />
+                        <ButtonNext />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+/* background */
+.trending_battle {
+    height: 1988px;
+    background-color: var(--rax-darkest);
+}
+
+/* Style */
+
+.half_container {
+    width: 50%;
+    height: 100%;
+    padding: 2.5rem;
+
+    .button_box {
+        justify-content: flex-end;
+    }
+}
+</style>
