c47ac75b55813aa57c16ae3c16be1e3993a96466
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-10 18:44:59 +0200

    Add draft of team member section

--- src/components/section/TeamMembers.vue
+++ src/components/section/TeamMembers.vue
@@ -11,6 +11,32 @@ export default {
         ButtonBack,
         SectionTitle,
     },
+    data() {
+        return {
+            cards: [
+                {
+                    image: 'public/img/team1.png',
+                    name: 'Cristiano Messi',
+                    role: 'CEO/Founder',
+                },
+                {
+                    image: 'public/img/team2.png',
+                    name: 'Sergio Naymer',
+                    role: 'Programmer',
+                },
+                {
+                    image: 'public/img/team3.png',
+                    name: 'Antonella Rocca',
+                    role: 'Project Manager',
+                },
+                {
+                    image: 'public/img/team4.png',
+                    name: 'Lionel Ramos',
+                    role: 'Game Designer',
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -30,8 +56,20 @@ export default {
                     </div>
                 </div>
             </div>
-            <div class="bottom_section">
-
+            <div class="bottom_section  d_flex">
+                <div v-for="(card, index) in cards" :key="index" class="tranding-games">
+                    <div class="box_card_game">
+                        <div class="box_img">
+                            <img :src="card.image" alt="">
+                        </div>
+                        <div class="box_card_text d_flex">
+                            <div class="description_game">
+                                <h3>{{ card.name }}</h3>
+                                <p>{{ card.role }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- /.bottom_section -->
         </div>
@@ -59,7 +97,48 @@ export default {
 
     .bottom_section {
         width: 300px;
-        height: 390px;
+        height: 400px;
+
+        .tranding-games {
+            width: 100%;
+            height: 290px;
+            margin: 1rem;
+
+            .box_img {
+                height: 100%;
+                position: relative;
+                overflow: hidden;
+                object-fit: cover;
+                object-position: center;
+
+                .badge {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    padding: 0.5rem;
+                    background-color: var(--rax-secondary);
+                    color: var(--rax-light);
+                }
+            }
+
+            .box_card_text {
+                padding: 1rem;
+                background-color: var(--rax-primary-dark);
+                justify-content: space-between;
+
+                .description_game>p {
+                    font-weight: 100;
+                    font-size: 1rem;
+                    padding-top: 5px;
+                }
+
+            }
+        }
     }
+}
+
+.box_card_game:hover box_card_text {
+    background-color: var(--rax-secondary);
+    color: var(--rax-darkest);
 }
 </style>
--- src/components/section/UpcomingMatch.vue
+++ src/components/section/UpcomingMatch.vue
@@ -2,7 +2,7 @@
 import ButtonNext from "../elements/ButtonNext.vue";
 import ButtonBack from "../elements/ButtonBack.vue";
 import SectionTitle from "../elements/SectionTitle.vue";
-
+import ButtonText from "../elements/ButtonText.vue"
 
 export default {
     name: 'UpcomingMatch',
@@ -10,7 +10,32 @@ export default {
         ButtonNext,
         ButtonBack,
         SectionTitle,
+        ButtonText,
     },
+    data() {
+        return {
+            cards: [
+                {
+                    image: 'public/img/m1.png',
+                    gamerName: 'Ninja Warrior Gaming',
+                    time: '9.45',
+                    date: '1st November, 2022',
+                },
+                {
+                    image: 'public/img/m2.png',
+                    gamerName: 'Witch Gaming',
+                    time: '9.45',
+                    date: '1st November, 2022',
+                },
+                {
+                    image: 'public/img/m3.png',
+                    gamerName: 'SkullG Gaming',
+                    time: '9.45',
+                    date: '1st November, 2022',
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -30,7 +55,29 @@ export default {
                     </div>
                 </div>
             </div>
-            <div class="bottom_section">
+
+            <div class="bottom_section d_flex">
+                <div v-for="(card, index) in cards" :key="index" class="match_cards">
+                    <div class="img_team">
+                        <img :src="card.image" alt="">
+                    </div>
+                    <div class="text_box_gamer">
+                        <h3>{{ card.gamerName }}</h3>
+                        <div class="button_generic_section d_flex">
+                            <button class="icon_btn ">
+                                <i class="fa-brands fa-youtube"></i>
+                            </button>
+                            <button class="icon_btn">
+                                <i class="fa-solid fa-message"></i>
+                            </button>
+                        </div>
+                        <div class="text_box_card">
+                            <h3 class="hours">{{ card.time }}</h3>
+                            <div class="date">{{ card.date }}</div>
+                        </div>
+                        <ButtonText title="View More" />
+                    </div>
+                </div>
 
             </div>
             <!-- /.bottom_section -->
@@ -45,20 +92,60 @@ export default {
     background-image: url(public/img/v-bg.png);
 }
 
-/* Style */
-
+/* Style title top section*/
 .half_container {
     width: 50%;
     height: 100%;
     padding: 2.5rem;
 
+
+    /* Next and Back Button */
     .button_box {
         justify-content: flex-end;
     }
+}
 
-    .bottom_section {
-        width: 300px;
-        height: 390px;
+/* Cards Style */
+.match_cards {
+    width: 420px;
+    height: 690px;
+    background-color: var(--rax-darker);
+    padding: 1rem;
+    border-radius: 10px;
+    margin: 0.5rem;
+
+
+    .img_team {
+        width: 350px;
+        height: 300px;
+        border-radius: 10px;
+        object-fit: cover;
+    }
+
+    .text_box_gamer {
+        text-align: center;
+    }
+}
+
+/* Buttons style */
+.icon_btn {
+    width: 3rem;
+    aspect-ratio: 1;
+    color: var(--rax-darkest);
+    background-color: var(--rax-highlight);
+    border-radius: 50%;
+    border: none;
+    margin: 0 0.4rem;
+
+
+    & i {
+        font-size: 1rem;
+    }
+
+    .battle_card:hover .icon_btn {
+        background-color: var(--rax-darker);
+        color: var(--rax-light);
+        border: none;
     }
 }
 </style>
